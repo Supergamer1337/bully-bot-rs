60c7f97ee44f7379680df2e6ade44316e54f0570
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2023-08-22 20:20:22 +0200

    Refactored to use a separate handler file

---
+++ src/handler.rs
@@ -0,0 +1,101 @@
+use rand::{seq::SliceRandom, Rng};
+use serenity::{model::prelude::*, prelude::*};
+use std::fs::DirEntry;
+
+pub struct Handler {
+    bully_chance: f64,
+    cringe_channels: Vec<u64>,
+    cringe_chance: f64,
+    assets: Vec<DirEntry>,
+}
+
+#[serenity::async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.author.bot {
+            return;
+        }
+
+        if !message_is_meme(&msg) {
+            return;
+        }
+
+        let num: f64 = rand::thread_rng().gen();
+        if self.cringe_channels.contains(&msg.channel_id.as_u64()) {
+            if num < self.cringe_chance {
+                return;
+            }
+
+            Self::send_cringe_message(&ctx, &msg).await;
+            return;
+        }
+
+        if num < self.bully_chance {
+            self.send_bully_message(&ctx, &msg).await;
+        }
+    }
+}
+
+impl Handler {
+    pub fn new(
+        bully_chance: f64,
+        assets: Vec<DirEntry>,
+        cringe_channels: Vec<u64>,
+        cringe_chance: f64,
+    ) -> Self {
+        Self {
+            bully_chance,
+            assets,
+            cringe_channels,
+            cringe_chance,
+        }
+    }
+
+    async fn send_bully_message(&self, ctx: &Context, msg: &Message) {
+        let video = match self.assets.choose(&mut rand::thread_rng()) {
+            Some(file) => file.path(),
+            None => {
+                println!("Failed to choose a bully file. Skipping message.");
+                return;
+            }
+        };
+
+        if let Err(why) = msg
+            .channel_id
+            .send_message(&ctx.http, |m| {
+                m.reference_message(msg);
+                m.add_file(&video);
+                m
+            })
+            .await
+        {
+            println!("Error sending message: {:?}", why);
+        }
+    }
+
+    async fn send_cringe_message(ctx: &Context, msg: &Message) {
+        if let Err(why) = msg
+            .channel_id
+            .send_message(&ctx.http, |m| {
+                m.reference_message(msg);
+                m.content("Honestly, this is cringe.");
+                m
+            })
+            .await
+        {
+            println!("Error sending message: {:?}", why);
+        }
+    }
+}
+
+fn message_is_meme(msg: &Message) -> bool {
+    if msg.embeds.len() > 0
+        || msg.content.contains("https://")
+        || msg.content.contains("http://")
+        || msg.attachments.len() > 0
+    {
+        true
+    } else {
+        false
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,14 @@
 use dotenv::dotenv;
-use rand::seq::SliceRandom;
-use rand::{thread_rng, Rng};
-use std::fs::DirEntry;
-use std::io;
-use std::{env, fs};
+use serenity::{framework::StandardFramework, model::prelude::*, Client};
+use std::{
+    env,
+    fs::{self, DirEntry},
+    io,
+};
 
-use serenity::async_trait;
-use serenity::framework::StandardFramework;
-use serenity::model::channel::Message;
-use serenity::model::prelude::*;
-use serenity::prelude::*;
+mod handler;
 
-struct Handler {
-    bully_chance: f64,
-    cringe_channels: Vec<u64>,
-    cringe_chance: f64,
-    assets: Vec<DirEntry>,
-}
-
-#[async_trait]
-impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.author.bot {
-            return;
-        }
-
-        if !message_is_meme(&msg) {
-            return;
-        }
-
-        let num: f64 = thread_rng().gen();
-        if self.cringe_channels.contains(&msg.channel_id.as_u64()) {
-            if num < self.cringe_chance {
-                return;
-            }
-
-            send_cringe_message(&ctx, &msg).await;
-            return;
-        }
-
-        if num < self.bully_chance {
-            send_bully_message(&ctx, &msg, &self).await;
-        }
-    }
-}
-
-fn message_is_meme(msg: &Message) -> bool {
-    if msg.embeds.len() > 0
-        || msg.content.contains("https://")
-        || msg.content.contains("http://")
-        || msg.attachments.len() > 0
-    {
-        true
-    } else {
-        false
-    }
-}
-
-async fn send_bully_message(ctx: &Context, msg: &Message, handler: &Handler) {
-    let video = match handler.assets.choose(&mut thread_rng()) {
-        Some(file) => file.path(),
-        None => {
-            println!("Failed to choose a bully file. Skipping message.");
-            return;
-        }
-    };
-
-    if let Err(why) = msg
-        .channel_id
-        .send_message(&ctx.http, |m| {
-            m.reference_message(msg);
-            m.add_file(&video);
-            m
-        })
-        .await
-    {
-        println!("Error sending message: {:?}", why);
-    }
-}
-
-async fn send_cringe_message(ctx: &Context, msg: &Message) {
-    if let Err(why) = msg
-        .channel_id
-        .send_message(&ctx.http, |m| {
-            m.reference_message(msg);
-            m.content("Honestly, this is cringe.");
-            m
-        })
-        .await
-    {
-        println!("Error sending message: {:?}", why);
-    }
-}
+use handler::Handler;
 
 #[tokio::main]
 async fn main() {
@@ -110,13 +27,15 @@ async fn main() {
 async fn create_discord_client(framework: StandardFramework) -> Client {
     let files = read_bully_files();
     let (token, intents) = get_token_and_intents();
+    let handler = Handler::new(
+        get_bully_chance(),
+        files,
+        get_cringe_channels(),
+        get_cringe_chance(),
+    );
+
     Client::builder(token, intents)
-        .event_handler(Handler {
-            bully_chance: get_bully_chance(),
-            cringe_channels: get_cringe_channels(),
-            cringe_chance: get_cringe_chance(),
-            assets: files,
-        })
+        .event_handler(handler)
         .framework(framework)
         .await
         .expect("Error creating client")
